11df3261f498bfccb71b533ab174cff4c51b7021
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-02-27 23:00:24 -0800

    added values method

--- hashtable.py
+++ hashtable.py
@@ -39,6 +39,12 @@ class HashTable(object):
         TODO: Running time: O(???) Why and under what conditions?"""
         # TODO: Loop through all buckets
         # TODO: Collect all values in each bucket
+        all_values = []
+        for bucket in self.buckets:
+            for key, value in bucket.items():
+                all_values.append(value)
+        return all_values
+
 
     def items(self):
         """Return a list of all items (key-value pairs) in this hash table.
